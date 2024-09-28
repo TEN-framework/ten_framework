ab463bf55175df38186f159b87f40efa9d6dc1f7
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-09-28 22:08:45 +0800

    refactor: refine extension stop flow

--- core/include_internal/ten_runtime/test/extension_test.h
+++ core/include_internal/ten_runtime/test/extension_test.h
@@ -6,39 +6,21 @@
 //
 #include "ten_runtime/ten_config.h"
 
-#include "include_internal/ten_runtime/extension/extension.h"
 #include "ten_runtime/ten_env_proxy/ten_env_proxy.h"
 
+// =-=-= 改名
 typedef struct ten_extension_test_t {
-  ten_extension_thread_t *test_extension_thread;
-  ten_extension_group_t *test_extension_group;
-  ten_list_t pre_created_extensions;
-  ten_thread_t *test_thread;
-} ten_extension_test_t;
-
-TEN_RUNTIME_API ten_extension_test_t *ten_extension_test_create(
-    ten_extension_t *test_extension, ten_extension_t *target_extension);
-
-TEN_RUNTIME_API void ten_extension_test_start(ten_extension_test_t *self);
-
-TEN_RUNTIME_API void ten_extension_test_wait(ten_extension_test_t *self);
-
-TEN_RUNTIME_API void ten_extension_test_destroy(ten_extension_test_t *self);
-
-typedef struct ten_extension_test_new_t {
   ten_thread_t *test_app_thread;
   ten_string_t test_extension_addon_name;
   ten_env_proxy_t *test_app_ten_env_proxy;
   ten_event_t *test_app_ten_env_proxy_create_completed;
-} ten_extension_test_new_t;
+} ten_extension_test_t;
 
-TEN_RUNTIME_API ten_extension_test_new_t *ten_extension_test_create_new(void);
+TEN_RUNTIME_API ten_extension_test_t *ten_extension_test_create(void);
 
-TEN_RUNTIME_API void ten_extension_test_destroy_new(
-    ten_extension_test_new_t *self);
+TEN_RUNTIME_API void ten_extension_test_destroy(ten_extension_test_t *self);
 
-TEN_RUNTIME_API void ten_extension_test_start_new(
-    ten_extension_test_new_t *self);
+TEN_RUNTIME_API void ten_extension_test_start(ten_extension_test_t *self);
 
-TEN_RUNTIME_API void ten_extension_test_add_addon(
-    ten_extension_test_new_t *self, const char *addon_name);
+TEN_RUNTIME_API void ten_extension_test_add_addon(ten_extension_test_t *self,
+                                                  const char *addon_name);
--- tests/ten_runtime/smoke/standalone_test/basic.cc
+++ tests/ten_runtime/smoke/standalone_test/basic.cc
@@ -4,15 +4,9 @@
 // Licensed under the Apache License, Version 2.0, with certain conditions.
 // Refer to the "LICENSE" file in the root directory for more information.
 //
-#include <nlohmann/json.hpp>
-#include <string>
-
 #include "gtest/gtest.h"
 #include "include_internal/ten_runtime/binding/cpp/ten.h"
 #include "include_internal/ten_runtime/test/extension_test.h"
-#include "ten_runtime/extension/extension.h"
-#include "ten_runtime/ten_env/internal/send.h"
-#include "ten_runtime/ten_env/ten_env.h"
 #include "ten_utils/lang/cpp/lib/value.h"
 #include "tests/ten_runtime/smoke/extension_test/util/binding/cpp/check.h"
 
@@ -41,83 +35,9 @@ TEN_CPP_REGISTER_ADDON_AS_EXTENSION(standalone_test_basic__test_extension_1,
 
 }  // namespace
 
-namespace {
-
-class test_ten_mock : public ten::ten_env_mock_t {
- public:
-  bool return_result(std::unique_ptr<ten::cmd_result_t> &&cmd,
-                     std::unique_ptr<ten::cmd_t> &&target_cmd,
-                     ten::error_t *err) override {
-    auto code = cmd->get_status_code();
-    auto message = cmd->get_property_string("detail");
-    if (test_case_num == 1) {
-      if (std::string(target_cmd->get_name()) == "hello_world") {
-        if (code == TEN_STATUS_CODE_OK) {
-          EXPECT_EQ(std::string(message), "hello world, too");
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
-  int64_t test_case_num{0};
-};
-
-void ten_cmd_result_handler(ten_extension_t *extension, ten_env_t *ten_env,
-                            ten_shared_ptr_t *cmd_result,
-                            void *cmd_result_handler_data) {
-  TEN_LOGE("receive result handler.");
-}
-
-void test_extension_on_start(ten_extension_t *self, ten_env_t *ten_env) {
-  std::unique_ptr<ten::cmd_t> test_cmd = ten::cmd_t::create_from_json(
-      R"({
-               "_ten": {
-                 "type": "cmd",
-                 "name": "test"
-               }
-             })");
-  ten_env_send_cmd(ten_env, test_cmd->get_underlying_msg(),
-                   ten_cmd_result_handler, nullptr, nullptr);
-}
-
-bool test_case_1() {
-  auto *ten_env_mock = new test_ten_mock();
-  return ten_env_mock->addon_create_extension_async(
-      "standalone_test_basic__test_extension_1",
-      "standalone_test_basic__test_extension_1",
-      [ten_env_mock](ten::ten_env_t &ten_env, ten::extension_t &extension) {
-        ten_extension_t *test_extension = ten_extension_create(
-            "test_extension", nullptr, nullptr, test_extension_on_start,
-            nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
-
-        ten_extension_test_t *test = ten_extension_test_create(
-            test_extension, extension.get_c_extension());
-
-        ten_extension_test_wait(test);
-
-        // ten_env_mock->test_case_num = 1;
-
-        // // Send a command to the extension.
-        // auto cmd = ten::cmd_t::create("hello_world");
-        // (static_cast<test_extension_1 *>(&extension))
-        //     ->on_cmd(ten, std::move(cmd));
-
-        ten_env_mock->addon_destroy_extension_async(
-            &extension,
-            [ten_env_mock](ten::ten_env_t &ten_env) { delete ten_env_mock; });
-        ten_extension_destroy(test_extension);
-
-        ten_extension_test_destroy(test);
-      });
-
-  return true;
-}
-
-}  // namespace
-
-TEST(StandaloneTest, Basic) {  // NOLINT
-  EXPECT_EQ(test_case_1(), true);
+TEST(StandaloneTest, DISABLED_Basic) {  // NOLINT
+  ten_extension_test_t *test = ten_extension_test_create();
+  ten_extension_test_add_addon(test, "standalone_test_basic__test_extension_1");
+  ten_extension_test_start(test);
+  ten_extension_test_destroy(test);
 }
