9228ff8ac773a699dc5a262756bda0a300956152
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-11-12 22:07:30 +0800

    feat: ensure some ten_env call timing

--- tests/ten_runtime/smoke/standalone_test/basic.cc
+++ tests/ten_runtime/smoke/standalone_test/basic.cc
@@ -53,6 +53,8 @@ class extension_tester_1 : public ten::extension_tester_t {
                          ten_env.stop_test();
                        }
                      });
+
+    ten_env.on_start_done();
   }
 };
 
