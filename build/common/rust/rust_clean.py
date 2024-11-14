67a44faf3a9a1af9dd00f76cb2947b0356514cac
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-11-14 23:48:48 +0800

    chore: add logic for ten_enable_clean_build

--- build/common/rust/rust_clean.py
+++ build/common/rust/rust_clean.py
@@ -7,75 +7,65 @@
 import argparse
 import sys
 import os
-from build.scripts import cmd_exec
+from build.scripts import cmd_exec, timestamp_proxy
 
 
 class ArgumentInfo(argparse.Namespace):
     def __init__(self):
         super().__init__()
         self.project_path: str
-        self.manifest_path: str
-        self.build_type: str
-        self.target: str
         self.target_path: str
-        self.env: list[str]
+        self.target: str
         self.log_level: int
+        self.tg_timestamp_proxy_file: str | None = None
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--project-path", type=str, required=True)
-    parser.add_argument("--manifest-path", type=str, required=False)
-    parser.add_argument("--build-type", type=str, required=True)
-    parser.add_argument("--target", type=str, required=True)
     parser.add_argument("--target-path", type=str, required=True)
-    parser.add_argument("--env", type=str, action="append", default=[])
+    parser.add_argument("--target", type=str, required=True)
     parser.add_argument("--log-level", type=int, required=True)
+    parser.add_argument(
+        "--tg-timestamp-proxy-file", type=str, default="", required=False
+    )
 
     arg_info = ArgumentInfo()
     args = parser.parse_args(namespace=arg_info)
 
-    # Setup environment variables.
-    for env in args.env:
-        split_key_index = str(env).find("=")
-        if split_key_index == -1:
-            sys.exit(1)
-        else:
-            os.environ[(str(env)[:split_key_index])] = str(env)[
-                split_key_index + len("=") :
-            ]
-
     origin_wd = os.getcwd()
 
+    returncode = 0
     try:
         os.chdir(args.project_path)
 
         cmd = [
             "cargo",
-            "build",
+            "clean",
             "--target-dir",
             args.target_path,
             "--target",
             args.target,
         ]
 
-        if args.manifest_path != "":
-            cmd.append(
-                "--manifest-path",
-            )
-            cmd.append(
-                args.manifest_path,
-            )
-
-        if args.build_type == "release":
-            cmd.append("--release")
-
-        returncode, _ = cmd_exec.run_cmd(cmd, args.log_level)
+        returncode, logs = cmd_exec.run_cmd(cmd, args.log_level)
         if returncode:
-            raise Exception("Failed to run cargo build.")
+            raise Exception(
+                f"Failed to run `cargo clean` on {args.project_path}: {logs}"
+            )
+        else:
+            print(logs)
+
+        # Success to clean the rust project, update the stamp file to represent
+        # this fact.
+        timestamp_proxy.touch_timestamp_proxy_file(args.tg_timestamp_proxy_file)
 
     except Exception as exc:
+        returncode = 1
+        timestamp_proxy.remove_timestamp_proxy_file(
+            args.tg_timestamp_proxy_file
+        )
         print(exc)
 
     finally:
