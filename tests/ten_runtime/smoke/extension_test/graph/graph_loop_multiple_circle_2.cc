e52cce902bc0395707ba39177a49c7bf2c22fe46
sunxilin <sunxilin@shengwang.cn>
2024-11-20 22:46:55 +0800

    fix: rename test file

--- tests/ten_runtime/smoke/extension_test/graph/graph_loop_multiple_circle_2.cc
+++ tests/ten_runtime/smoke/extension_test/graph/graph_loop_multiple_circle_2.cc
@@ -98,7 +98,7 @@ TEN_CPP_REGISTER_ADDON_AS_EXTENSION(
 
 }  // namespace
 
-TEST(ExtensionTest, GraphLoopMultipleCircle2) {  // NOLINT
+TEST(ExtensionTest, DISABLED_GraphLoopMultipleCircle2) {  // NOLINT
   // Start app.
   auto *app_thread =
       ten_thread_create("app thread", test_app_thread_main, nullptr);
