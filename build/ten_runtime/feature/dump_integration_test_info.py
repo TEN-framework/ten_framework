6630139b285a8b2d36d7b84f7ab20ff2ec73b45c
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-12-07 11:51:56 +0800

    fix: refine codes

--- build/ten_runtime/feature/dump_integration_test_info.py
+++ build/ten_runtime/feature/dump_integration_test_info.py
@@ -43,46 +43,24 @@ def dump_info_json(args: ArgumentInfo):
     if not os.path.exists(resource_dir):
         os.makedirs(resource_dir, exist_ok=True)
 
-    with open(info_file, "w") as f:
-        f.write(json.dumps(info, ensure_ascii=False))
+    with open(info_file, "w", encoding="utf-8") as f:
+        json.dump(info, f, ensure_ascii=False, indent=2)
 
 
-def copy_files_to_be_replaced_after_install_app(args: ArgumentInfo):
-    if args.replace_files_after_install_app is None:
+def copy_replacement_files(
+    args: ArgumentInfo, replaced_files: list[str], dest_dir: str
+):
+    if replaced_files is None:
         return
 
     out_dir = os.path.join(
         args.test_case_out_dir,
         assemble_info_dir,
         args.generated_app_src_root_dir_name,
-        "files_to_be_replaced_after_install_app",
+        dest_dir,
     )
 
-    for file in args.replace_files_after_install_app:
-        src_file = os.path.join(args.test_case_src_dir, file)
-        dst_file = os.path.join(out_dir, file)
-        if not os.path.exists(src_file):
-            print(f"File {src_file} does not exist")
-            continue
-
-        if not os.path.exists(os.path.dirname(dst_file)):
-            os.makedirs(os.path.dirname(dst_file), exist_ok=True)
-
-        fs_utils.copy(src_file, dst_file)
-
-
-def copy_files_to_be_replaced_after_install_all(args: ArgumentInfo):
-    if args.replace_files_after_install_all is None:
-        return
-
-    out_dir = os.path.join(
-        args.test_case_out_dir,
-        assemble_info_dir,
-        args.generated_app_src_root_dir_name,
-        "files_to_be_replaced_after_install_all",
-    )
-
-    for file in args.replace_files_after_install_all:
+    for file in replaced_files:
         src_file = os.path.join(args.test_case_src_dir, file)
         dst_file = os.path.join(out_dir, file)
         if not os.path.exists(src_file):
@@ -150,11 +128,18 @@ if __name__ == "__main__":
     arg_info = ArgumentInfo()
     args = parser.parse_args(namespace=arg_info)
 
-    # dump the full json to root_dir/.assembly_resources/info.json
     dump_info_json(args)
 
-    copy_files_to_be_replaced_after_install_app(args)
+    copy_replacement_files(
+        args,
+        args.replace_files_after_install_app,
+        "files_to_be_replaced_after_install_app",
+    )
 
-    copy_files_to_be_replaced_after_install_all(args)
+    copy_replacement_files(
+        args,
+        args.replace_files_after_install_all,
+        "files_to_be_replaced_after_install_all",
+    )
 
     sys.exit(0)
