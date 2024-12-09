83e8d7d4110ffbda76d9e4d7e269459ccecfa9ed
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-12-09 22:39:43 +0800

    fix: refine codes

--- core/src/ten_manager/src/solver/solve.rs
+++ core/src/ten_manager/src/solver/solve.rs
@@ -33,20 +33,20 @@ fn get_model(
         .symbols(ShowType::SHOWN)
         .expect("Failed to retrieve symbols in the model.");
 
-    tman_verbose_println!(tman_config, "Model:");
+    // tman_verbose_println!(tman_config, "Model:");
 
     let mut result = Vec::new();
     *is_usable = true;
 
     for symbol in atoms {
-        tman_verbose_println!(tman_config, " {}", symbol);
+        // tman_verbose_println!(tman_config, " {}", symbol);
 
         result.push(symbol.to_string());
         if symbol.to_string().starts_with("error(") {
             *is_usable = false;
         }
     }
-    tman_verbose_println!(tman_config, "");
+    // tman_verbose_println!(tman_config, "");
 
     Some(result)
 }
@@ -197,8 +197,8 @@ fn solve(
         conf.value_set(heuristic_key, "berkmin")
             .expect("Failed to set heuristic to berkmin.");
 
-        print_configuration(tman_config, conf, root_key, 0);
-        tman_verbose_println!(tman_config, "");
+        // print_configuration(tman_config, conf, root_key, 0);
+        // tman_verbose_println!(tman_config, "");
     }
 
     let main_program = include_str!("main.lp");
@@ -280,7 +280,7 @@ fn solve(
     // recursively.
     let stats = ctl.statistics().unwrap();
     let stats_key = stats.root().unwrap();
-    print_statistics(tman_config, stats, stats_key, 0);
+    // print_statistics(tman_config, stats, stats_key, 0);
 
     Ok((usable_model, non_usable_models))
 }
