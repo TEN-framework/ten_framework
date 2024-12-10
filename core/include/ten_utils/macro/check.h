513d22d6f2ec5fe09dcc9f2832438e401bf537b6
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-12-10 15:50:57 +0800

    feat: enable python on mac (#379)
    * feat: enable python on mac
    
    * feat: enable python on mac
    
    * feat: enable python on mac
    
    * feat: enable python on mac
    
    * chore: refine go codes
    
    * fix: remove python installation in github CI workflow
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    * fix: refine codes
    
    ---------
    
    Co-authored-by: sunxilin <sunxilin@shengwang.cn>

--- core/include/ten_utils/macro/check.h
+++ core/include/ten_utils/macro/check.h
@@ -36,40 +36,53 @@
 
 // TEN_ASSERT is used within `ten_string_t`, so do not use `ten_string_t` inside
 // `TEN_ASSERT` to avoid circular dependencies.
-#define ASSERT_ERR_MSG_MAX_LENGTH 1024
+//
+// This size should not be too large; otherwise, it may cause a stack overflow
+// in the deep call chain.
+#define ASSERT_ERR_MSG_MAX_LENGTH 128
 
 #ifndef NDEBUG
 
-#define TEN_ASSERT(expr, fmt, ...)                                          \
-  do {                                                                      \
-    /* NOLINTNEXTLINE */                                                    \
-    if (!(expr)) {                                                          \
-      /* NOLINTNEXTLINE */                                                  \
-      char ____err_msg[ASSERT_ERR_MSG_MAX_LENGTH];                          \
-      (void)snprintf(____err_msg, sizeof(____err_msg), fmt, ##__VA_ARGS__); \
-      (void)fprintf(stderr, "%s\n", ____err_msg);                           \
-      ten_backtrace_dump_global(0);                                         \
-      /* NOLINTNEXTLINE */                                                  \
-      assert(0);                                                            \
-    }                                                                       \
+#define TEN_ASSERT(expr, fmt, ...)                                        \
+  do {                                                                    \
+    /* NOLINTNEXTLINE */                                                  \
+    if (!(expr)) {                                                        \
+      /* NOLINTNEXTLINE */                                                \
+      char ____err_msg[ASSERT_ERR_MSG_MAX_LENGTH];                        \
+      int written =                                                       \
+          snprintf(____err_msg, sizeof(____err_msg), fmt, ##__VA_ARGS__); \
+      assert(written > 0);                                                \
+      written = fprintf(stderr, "%s\n", ____err_msg);                     \
+      assert(written > 0);                                                \
+      ten_backtrace_dump_global(0);                                       \
+      /* NOLINTNEXTLINE */                                                \
+      assert(0);                                                          \
+    }                                                                     \
   } while (0)
 
 #else  // NDEBUG
 
 // Enable minimal protection if the optimization is enabled.
 
-#define TEN_ASSERT(expr, fmt, ...)                                          \
-  do {                                                                      \
-    /* NOLINTNEXTLINE */                                                    \
-    if (!(expr)) {                                                          \
-      /* NOLINTNEXTLINE */                                                  \
-      char ____err_msg[ASSERT_ERR_MSG_MAX_LENGTH];                          \
-      (void)snprintf(____err_msg, sizeof(____err_msg), fmt, ##__VA_ARGS__); \
-      (void)fprintf(stderr, "%s\n", ____err_msg);                           \
-      ten_backtrace_dump_global(0);                                         \
-      /* NOLINTNEXTLINE */                                                  \
-      abort();                                                              \
-    }                                                                       \
+#define TEN_ASSERT(expr, fmt, ...)                                        \
+  do {                                                                    \
+    /* NOLINTNEXTLINE */                                                  \
+    if (!(expr)) {                                                        \
+      /* NOLINTNEXTLINE */                                                \
+      char ____err_msg[ASSERT_ERR_MSG_MAX_LENGTH];                        \
+      int written =                                                       \
+          snprintf(____err_msg, sizeof(____err_msg), fmt, ##__VA_ARGS__); \
+      if (written <= 0) {                                                 \
+        abort();                                                          \
+      }                                                                   \
+      written = fprintf(stderr, "%s\n", ____err_msg);                     \
+      if (written <= 0) {                                                 \
+        abort();                                                          \
+      }                                                                   \
+      ten_backtrace_dump_global(0);                                       \
+      /* NOLINTNEXTLINE */                                                \
+      abort();                                                            \
+    }                                                                     \
   } while (0)
 
 #endif  // NDEBUG
