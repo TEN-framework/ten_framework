2abbb1b8dd87b74d2e76afdcbb5b6c0ade07d0ad
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-12-10 12:13:31 +0800

    fix: refine codes

--- core/src/ten_utils/lib/sys/posix/shm.c
+++ core/src/ten_utils/lib/sys/posix/shm.c
@@ -19,7 +19,7 @@
 
 static char *__make_abs_path(const char *name) {
   char *abs_path = NULL;
-  long abs_path_size = 0;
+  size_t abs_path_size = 0;
 
   if (name == NULL) {
     return NULL;
@@ -41,7 +41,9 @@ static char *__make_abs_path(const char *name) {
     return NULL;
   }
 
-  snprintf(abs_path, abs_path_size, "/%s", name);
+  int written = snprintf(abs_path, abs_path_size, "/%s", name);
+  assert(written > 0);
+
   return abs_path;
 }
 
