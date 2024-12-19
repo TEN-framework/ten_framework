dc86398053efae0dacffcdae31dca8952b19a80d
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-12-20 00:01:28 +0800

    chore: add more test cases

--- tests/ten_runtime/smoke/msg_test/msg_13.cc
+++ tests/ten_runtime/smoke/msg_test/msg_13.cc
@@ -30,7 +30,7 @@ class test_extension_1 : public ten::extension_t {
                       "type": "extension",
                       "name": "msg_13__extension_1",
                       "version": "0.1.0",
-                                          "api": {
+                        "api": {
                         "cmd_out": [
                           {
                             "name": "test",
