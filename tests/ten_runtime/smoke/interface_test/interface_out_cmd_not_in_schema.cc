6043ea024a13e9ae44befd5c092653993310e968
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-12-20 14:30:52 +0800

    test: add more test cases

--- tests/ten_runtime/smoke/interface_test/interface_out_cmd_not_in_schema.cc
+++ tests/ten_runtime/smoke/interface_test/interface_out_cmd_not_in_schema.cc
@@ -26,7 +26,7 @@ class test_extension_1 : public ten::extension_t {
                                 "type": "extension",
                                 "name": "test_extension_1",
                                 "version": "0.1.0",
-                                  "api": {
+                                "api": {
                                   "interface_out": [
                                     {
                                       "name": "ia",
