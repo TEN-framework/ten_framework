f95c310ac014a2574d36b945a8a215a67e112716
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2024-12-26 11:38:24 +0800

    refactor: refine some codes

--- core/include_internal/ten_runtime/extension_group/extension_group.h
+++ core/include_internal/ten_runtime/extension_group/extension_group.h
@@ -13,7 +13,6 @@
 #include "include_internal/ten_runtime/binding/common.h"
 #include "include_internal/ten_runtime/metadata/metadata.h"
 #include "ten_runtime/binding/common.h"
-#include "ten_runtime/extension_group/extension_group.h"
 #include "ten_utils/container/list.h"
 #include "ten_utils/io/runloop.h"
 #include "ten_utils/lib/signature.h"
@@ -32,6 +31,21 @@ typedef struct ten_app_t ten_app_t;
 typedef struct ten_addon_host_t ten_addon_host_t;
 typedef struct ten_env_t ten_env_t;
 typedef struct ten_extension_group_info_t ten_extension_group_info_t;
+
+typedef void (*ten_extension_group_on_configure_func_t)(
+    ten_extension_group_t *self, ten_env_t *ten_env);
+
+typedef void (*ten_extension_group_on_init_func_t)(ten_extension_group_t *self,
+                                                   ten_env_t *ten_env);
+
+typedef void (*ten_extension_group_on_deinit_func_t)(
+    ten_extension_group_t *self, ten_env_t *ten_env);
+
+typedef void (*ten_extension_group_on_create_extensions_func_t)(
+    ten_extension_group_t *self, ten_env_t *ten_env);
+
+typedef void (*ten_extension_group_on_destroy_extensions_func_t)(
+    ten_extension_group_t *self, ten_env_t *ten_env, ten_list_t extensions);
 
 typedef enum TEN_EXTENSION_GROUP_STATE {
   TEN_EXTENSION_GROUP_STATE_INIT,
@@ -79,6 +93,22 @@ typedef struct ten_extension_group_t {
   size_t extensions_cnt_of_being_destroyed;
 } ten_extension_group_t;
 
+TEN_RUNTIME_PRIVATE_API bool ten_extension_group_check_integrity(
+    ten_extension_group_t *self, bool check_thread);
+
+TEN_RUNTIME_PRIVATE_API ten_extension_group_t *ten_extension_group_create(
+    const char *name, ten_extension_group_on_configure_func_t on_configure,
+    ten_extension_group_on_init_func_t on_init,
+    ten_extension_group_on_deinit_func_t on_deinit,
+    ten_extension_group_on_create_extensions_func_t on_create_extensions,
+    ten_extension_group_on_destroy_extensions_func_t on_destroy_extensions);
+
+TEN_RUNTIME_PRIVATE_API void ten_extension_group_destroy(
+    ten_extension_group_t *self);
+
+TEN_RUNTIME_PRIVATE_API ten_env_t *ten_extension_group_get_ten_env(
+    ten_extension_group_t *self);
+
 TEN_RUNTIME_PRIVATE_API void ten_extension_group_create_extensions(
     ten_extension_group_t *self);
 
@@ -106,14 +136,15 @@ TEN_RUNTIME_PRIVATE_API void ten_extension_group_on_destroy_extensions_done(
 TEN_RUNTIME_PRIVATE_API void ten_extension_group_on_create_extensions_done(
     ten_extension_group_t *self, ten_list_t *extensions);
 
-TEN_RUNTIME_API ten_list_t *
+TEN_RUNTIME_PRIVATE_API ten_list_t *
 ten_extension_group_get_extension_addon_and_instance_name_pairs(
     ten_extension_group_t *self);
 
-TEN_RUNTIME_API void ten_extension_group_set_extension_cnt_of_being_destroyed(
+TEN_RUNTIME_PRIVATE_API void
+ten_extension_group_set_extension_cnt_of_being_destroyed(
     ten_extension_group_t *self, size_t new_cnt);
 
-TEN_RUNTIME_API size_t
+TEN_RUNTIME_PRIVATE_API size_t
 ten_extension_group_decrement_extension_cnt_of_being_destroyed(
     ten_extension_group_t *self);
 
