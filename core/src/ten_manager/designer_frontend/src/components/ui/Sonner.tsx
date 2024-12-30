1c0ae993a06ae2df4dd20f2bd29d1c4b6d238209
czhen <56986964+shczhen@users.noreply.github.com>
2024-12-30 18:43:05 +0800

    fix: editor context menu and popup

--- core/src/ten_manager/designer_frontend/src/components/ui/Sonner.tsx
+++ core/src/ten_manager/designer_frontend/src/components/ui/Sonner.tsx
@@ -1,6 +1,13 @@
 /* eslint-disable max-len */
 import { useTheme } from "next-themes";
 import { Toaster as Sonner } from "sonner";
+import {
+  TriangleAlertIcon,
+  CircleCheckIcon,
+  InfoIcon,
+  LoaderCircleIcon,
+  CircleXIcon,
+} from "lucide-react";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
@@ -21,6 +28,15 @@ const Toaster = ({ ...props }: ToasterProps) => {
           cancelButton:
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
+      }}
+      icons={{
+        success: <CircleCheckIcon className="h-4 w-4 text-green-500" />,
+        info: <InfoIcon className="h-4 w-4 text-blue-500" />,
+        warning: <TriangleAlertIcon className="h-4 w-4 text-amber-500" />,
+        error: <CircleXIcon className="h-4 w-4 text-red-500" />,
+        loading: (
+          <LoaderCircleIcon className="h-4 w-4 text-gray-500 animate-spin" />
+        ),
       }}
       {...props}
     />
