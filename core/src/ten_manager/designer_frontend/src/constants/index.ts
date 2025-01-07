3642d922855db76364b553f7e9c4317a54711073
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-01-07 18:09:03 +0800

    fix: refine codes

--- core/src/ten_manager/designer_frontend/src/constants/index.ts
+++ core/src/ten_manager/designer_frontend/src/constants/index.ts
@@ -1,2 +1,8 @@
+//
+// Copyright © 2025 Agora
+// This file is part of TEN Framework, an open source project.
+// Licensed under the Apache License, Version 2.0, with certain conditions.
+// Refer to the "LICENSE" file in the root directory for more information.
+//
 export const TEN_FRAMEWORK_URL = "https://www.theten.ai/";
 export const TEN_FRAMEWORK_GITHUB_URL = "https://github.com/TEN-framework/";
