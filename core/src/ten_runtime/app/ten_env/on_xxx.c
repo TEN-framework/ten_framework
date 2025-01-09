ad7a8b6e40596671dfc4063e42f80a854e949e0a
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-01-09 13:47:38 +0800

    feat: gracefully exit the app if auto start predefined_graph fail to start

--- core/src/ten_runtime/app/ten_env/on_xxx.c
+++ core/src/ten_runtime/app/ten_env/on_xxx.c
@@ -187,8 +187,8 @@ void ten_app_on_configure_done(ten_env_t *ten_env) {
   if (ten_string_is_empty(&self->uri)) {
     ten_string_set_from_c_str(&self->uri, TEN_STR_LOCALHOST);
   }
-  l ten_addon_load_all_from_app_base_dir(
-      ten_string_get_raw_str(&self->base_dir), &err);
+  ten_addon_load_all_from_app_base_dir(ten_string_get_raw_str(&self->base_dir),
+                                       &err);
   ten_addon_load_all_from_ten_package_base_dirs(&self->ten_package_base_dirs,
                                                 &err);
 
