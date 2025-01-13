1e1541d7b671f81504fa303820264455532ed1c5
xxxxl_sun <31622273+sunxilin@users.noreply.github.com>
2025-01-13 14:39:45 +0800

    fix: complete PYTHONPATH for standalone testing (#551)
    Co-authored-by: Hu Yueh-Wei <wei.hu.tw@gmail.com>

--- tests/ten_runtime/integration/python/standalone_test_async_python/default_async_extension_python/tests/test_mock.py
+++ tests/ten_runtime/integration/python/standalone_test_async_python/default_async_extension_python/tests/test_mock.py
@@ -4,7 +4,6 @@
 # Licensed under the Apache License, Version 2.0, with certain conditions.
 # Refer to the "LICENSE" file in the root directory for more information.
 #
-from pathlib import Path
 from typing import Optional
 from ten import (
     ExtensionTester,
