65eb49d4b01a5ddc4395e5f82d2d3b873404c758
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-01-23 16:01:33 +0800

    chore: refine ffmpeg extensions

--- packages/example_extensions/ffmpeg_muxer/src/muxer.h
+++ packages/example_extensions/ffmpeg_muxer/src/muxer.h
@@ -46,11 +46,12 @@ class muxer_t {
   muxer_t();
   ~muxer_t();
 
+  // @{
   muxer_t(const muxer_t &other) = delete;
   muxer_t(muxer_t &&other) = delete;
-
   muxer_t &operator=(const muxer_t &other) = delete;
   muxer_t &operator=(muxer_t &&other) = delete;
+  // @}
 
   bool open(const std::string &dest_name, bool realtime);
 
@@ -64,77 +65,77 @@ class muxer_t {
  private:
   friend class muxer_thread_t;
 
-  bool is_av_encoder_opened_();
+  bool is_av_encoder_opened();
 
-  bool open_video_encoder_(bool realtime);
-  bool open_audio_encoder_();
+  bool open_video_encoder(bool realtime);
+  bool open_audio_encoder();
 
-  void dump_video_info_();
-  void dump_audio_info_();
+  void dump_video_info();
+  void dump_audio_info();
 
-  int64_t next_video_pts_();
-  int64_t next_audio_pts_();
+  int64_t next_video_pts();
+  int64_t next_audio_pts();
 
-  bool encode_audio_frame_(AVFrame *frame);
-  bool encode_audio_silent_frame_();
+  bool encode_audio_frame(AVFrame *frame);
+  bool encode_audio_silent_frame();
 
   void flush_remaining_audio_frames();
   void flush_remaining_video_frames();
 
-  bool allocate_audio_fifo_(AVCodecParameters *encoded_stream_params);
-  bool allocate_audio_frame_(AVCodecParameters *encoded_stream_params);
+  bool allocate_audio_fifo(AVCodecParameters *encoded_stream_params);
+  bool allocate_audio_frame(AVCodecParameters *encoded_stream_params);
 
-  bool convert_audio_frame_(AVCodecParameters *encoded_stream_params,
-                            ten::audio_frame_t &ten_audio_frame);
-  AVFrame *convert_video_frame_(ten::video_frame_t &video_frame);
+  bool convert_audio_frame(AVCodecParameters *encoded_stream_params,
+                           ten::audio_frame_t &ten_audio_frame);
+  AVFrame *convert_video_frame(ten::video_frame_t &video_frame);
 
-  bool create_audio_converter_(AVCodecParameters *encoded_stream_params,
-                               ten::audio_frame_t &ten_audio_frame);
-  bool create_video_converter_(ten::video_frame_t &video_frame);
+  bool create_audio_converter(AVCodecParameters *encoded_stream_params,
+                              ten::audio_frame_t &ten_audio_frame);
+  bool create_video_converter(ten::video_frame_t &video_frame);
 
   // @{
   // Source video settings.
-  int src_video_width_;
-  int src_video_height_;
-  int64_t src_video_bit_rate_;
-  int64_t src_video_number_of_frames_;
-  AVRational src_video_frame_rate_;
-  AVRational src_video_time_base_;
+  int src_video_width;
+  int src_video_height;
+  int64_t src_video_bit_rate;
+  int64_t src_video_number_of_frames;
+  AVRational src_video_frame_rate;
+  AVRational src_video_time_base;
   // @}
 
   // @{
   // Source audio settings.
-  int src_audio_sample_rate_;
-  AVRational src_audio_time_base_;
-  uint64_t src_audio_channel_layout_;
+  int src_audio_sample_rate;
+  AVRational src_audio_time_base;
+  uint64_t src_audio_channel_layout;
   // @}
 
-  std::string dest_name_;
+  std::string dest_name;
 
-  AVFormatContext *output_format_ctx_;
+  AVFormatContext *output_format_ctx;
 
-  unsigned int video_stream_idx_;
-  unsigned int audio_stream_idx_;
+  unsigned int video_stream_idx;
+  unsigned int audio_stream_idx;
 
-  int64_t next_video_idx_;  // Next video frame index in video encoder context.
-  int64_t next_audio_idx_;  // Next audio sample index in audio encoder context.
+  int64_t next_video_idx;  // Next video frame index in video encoder context.
+  int64_t next_audio_idx;  // Next audio sample index in audio encoder context.
 
-  AVCodecContext *video_encoder_ctx_;
-  AVCodecContext *audio_encoder_ctx_;
+  AVCodecContext *video_encoder_ctx;
+  AVCodecContext *audio_encoder_ctx;
 
-  const AVCodec *video_encoder_;
-  const AVCodec *audio_encoder_;
+  const AVCodec *video_encoder;
+  const AVCodec *audio_encoder;
 
-  AVStream *video_stream_;
-  AVStream *audio_stream_;
+  AVStream *video_stream;
+  AVStream *audio_stream;
 
-  struct SwsContext *video_converter_ctx_;
-  struct SwrContext *audio_converter_ctx_;
+  struct SwsContext *video_converter_ctx;
+  struct SwrContext *audio_converter_ctx;
 
-  AVPacket *packet_;
-  AVAudioFifo *audio_fifo_;
-  AVFrame *audio_frame_;
-  uint64_t audio_prepend_pts_;
+  AVPacket *packet;
+  AVAudioFifo *audio_fifo;
+  AVFrame *audio_frame;
+  uint64_t audio_prepend_pts;
 };
 
 }  // namespace ffmpeg_extension
--- packages/example_extensions/ffmpeg_muxer/src/muxer_thread.cc
+++ packages/example_extensions/ffmpeg_muxer/src/muxer_thread.cc
@@ -19,87 +19,78 @@
 namespace ten {
 namespace ffmpeg_extension {
 
-enum {
-  // Audio continuity is very important, and the data is relatively small, so
-  // try not to drop it, and to accumulate up to the following amount in the
-  // queue.
-  AUDIO_FRAME_FIFO_SIZE = 10000,
-
-  VIDEO_FRAME_FIFO_SIZE = 10000,
-};
-
 muxer_thread_t::muxer_thread_t(ten::ten_env_proxy_t *ten_env_proxy,
                                demuxer_settings_t &settings,
                                std::string output_stream)
-    : muxer_thread_(nullptr),
-      muxer_thread_is_started_(ten_event_create(0, 0)),
-      stop_(0),
-      muxer_(nullptr),
-      out_lock_(ten_mutex_create()),
-      out_available_(ten_event_create(0, 1)),
+    : muxer_thread(nullptr),
+      muxer_thread_is_started(ten_event_create(0, 0)),
+      is_stop(0),
+      muxer(nullptr),
+      out_lock(ten_mutex_create()),
+      out_available(ten_event_create(0, 1)),
       settings(settings),
-      output_stream_(std::move(output_stream)),
-      audio_eof_(false),
-      video_eof_(false),
-      ten_env_proxy_(ten_env_proxy) {}
+      output_stream(std::move(output_stream)),
+      audio_eof(false),
+      video_eof(false),
+      ten_env_proxy(ten_env_proxy) {}
 
 muxer_thread_t::~muxer_thread_t() {
-  ten_mutex_destroy(out_lock_);
-  ten_event_destroy(out_available_);
+  ten_mutex_destroy(out_lock);
+  ten_event_destroy(out_available);
 
-  out_audios_.clear();
-  out_images_.clear();
+  out_audios.clear();
+  out_images.clear();
 
   // Clear muxer relevant resources.
-  ten_event_destroy(muxer_thread_is_started_);
+  ten_event_destroy(muxer_thread_is_started);
 
-  delete muxer_;
-  delete ten_env_proxy_;
+  delete muxer;
+  delete ten_env_proxy;
 
   TEN_LOGD("All the muxer resources have been cleaned.");
 }
 
-void muxer_thread_t::wait_for_the_first_av_frame_() {
-  while ((ten_atomic_load(&stop_) == 0) && out_audios_.empty() &&
-         out_images_.empty()) {
-    int rc = ten_mutex_lock(out_lock_);
+void muxer_thread_t::wait_for_the_first_av_frame() {
+  while ((ten_atomic_load(&is_stop) == 0) && out_audios.empty() &&
+         out_images.empty()) {
+    int rc = ten_mutex_lock(out_lock);
     TEN_ASSERT(!rc, "Should not happen.");
 
-    if (out_audios_.empty() && out_images_.empty()) {
-      rc = ten_mutex_unlock(out_lock_);
+    if (out_audios.empty() && out_images.empty()) {
+      rc = ten_mutex_unlock(out_lock);
       TEN_ASSERT(!rc, "Should not happen.");
 
       TEN_LOGD("No further frames need to be muxed, wait...");
 
-      rc = ten_event_wait(out_available_, -1);
+      rc = ten_event_wait(out_available, -1);
       TEN_ASSERT(!rc, "Should not happen.");
 
       continue;
     }
 
-    rc = ten_mutex_unlock(out_lock_);
+    rc = ten_mutex_unlock(out_lock);
     TEN_ASSERT(!rc, "Should not happen.");
   }
 }
 
-void *muxer_thread_main_(void *self_) {
+void *muxer_thread_main(void *self_) {
   TEN_LOGD("Muxer thread is started.");
 
   auto *self = static_cast<muxer_thread_t *>(self_);
   TEN_ASSERT(self, "Invalid argument.");
 
-  if (ten_atomic_load(&self->stop_) != 0) {
+  if (ten_atomic_load(&self->is_stop) != 0) {
     // Muxer thread has already been triggered to stop.
     return nullptr;
   }
 
-  self->create_muxer_();
+  self->create_muxer();
 
-  ten_event_set(self->muxer_thread_is_started_);
+  ten_event_set(self->muxer_thread_is_started);
 
   // Wait for the first frame to come before starting, to avoid meaningless CPU
   // resource wastage.
-  self->wait_for_the_first_av_frame_();
+  self->wait_for_the_first_av_frame();
 
   TEN_LOGD("Starting to mux...");
 
@@ -110,49 +101,49 @@ void *muxer_thread_main_(void *self_) {
   std::list<std::unique_ptr<ten::audio_frame_t>> out_audios;
   std::list<std::unique_ptr<ten::video_frame_t>> out_images;
 
-  while ((ten_atomic_load(&self->stop_) == 0 || !self->out_audios_.empty() ||
-          !self->out_images_.empty()) &&
-         status && !(self->audio_eof_ && self->video_eof_)) {
+  while ((ten_atomic_load(&self->is_stop) == 0 || !self->out_audios.empty() ||
+          !self->out_images.empty()) &&
+         status && !(self->audio_eof && self->video_eof)) {
     out_audios.clear();
     out_images.clear();
 
     {
       // Get the received audio + video frames at once.
 
-      int rc = ten_mutex_lock(self->out_lock_);
+      int rc = ten_mutex_lock(self->out_lock);
       TEN_ASSERT(!rc, "Should not happen.");
 
-      out_audios.swap(self->out_audios_);
-      out_images.swap(self->out_images_);
+      out_audios.swap(self->out_audios);
+      out_images.swap(self->out_images);
 
-      rc = ten_mutex_unlock(self->out_lock_);
+      rc = ten_mutex_unlock(self->out_lock);
       TEN_ASSERT(!rc, "Should not happen.");
     }
 
     for (auto &out_audio : out_audios) {
       auto encode_status =
-          self->muxer_->encode_audio_frame(std::move(out_audio));
+          self->muxer->encode_audio_frame(std::move(out_audio));
       status = encode_status != ENCODE_STATUS_ERROR;
       TEN_ASSERT(status, "Should not happen.");
 
       if (encode_status == ENCODE_STATUS_EOF) {
-        self->audio_eof_ = true;
+        self->audio_eof = true;
       }
     }
 
     for (auto &out_image : out_images) {
       auto encode_status =
-          self->muxer_->encode_video_frame(std::move(out_image));
+          self->muxer->encode_video_frame(std::move(out_image));
       status = encode_status != ENCODE_STATUS_ERROR;
       TEN_ASSERT(status, "Should not happen.");
 
       if (encode_status == ENCODE_STATUS_EOF) {
-        self->video_eof_ = true;
+        self->video_eof = true;
       }
 
       if (status) {
         int64_t now = ten_current_time();
-        int64_t expected_time = start_time + self->muxer_->next_video_timing();
+        int64_t expected_time = start_time + self->muxer->next_video_timing();
         int64_t sleep_time = expected_time - now - sleep_overhead;
         if (sleep_time > 0) {
           ten_sleep(sleep_time);
@@ -178,99 +169,87 @@ void *muxer_thread_main_(void *self_) {
 }
 
 void muxer_thread_t::start() {
-  muxer_thread_ = ten_thread_create(nullptr, muxer_thread_main_, this);
-  TEN_ASSERT(muxer_thread_, "Invalid argument.");
+  muxer_thread = ten_thread_create(nullptr, muxer_thread_main, this);
+  TEN_ASSERT(muxer_thread, "Invalid argument.");
 }
 
 void muxer_thread_t::wait_for_start() {
-  ten_event_wait(muxer_thread_is_started_, -1);
+  ten_event_wait(muxer_thread_is_started, -1);
 }
 
 void muxer_thread_t::stop() {
   TEN_LOGD("Signal muxer thread to close.");
 
-  ten_atomic_store(&stop_, 1);
+  ten_atomic_store(&is_stop, 1);
 
   // Kick muxer thread in case it is waiting for the first av frame.
-  ten_event_set(out_available_);
+  ten_event_set(out_available);
 }
 
 void muxer_thread_t::notify_completed(bool success) {
   auto cmd = ten::cmd_t::create("complete");
-  cmd->set_property("input_stream", output_stream_);
+  cmd->set_property("input_stream", output_stream);
   cmd->set_property("success", success);
 
   auto cmd_shared =
       std::make_shared<std::unique_ptr<ten::cmd_t>>(std::move(cmd));
 
-  ten_env_proxy_->notify([cmd_shared](ten::ten_env_t &ten_env) {
+  ten_env_proxy->notify([cmd_shared](ten::ten_env_t &ten_env) {
     ten_env.send_cmd(std::move(*cmd_shared));
   });
 }
 
 void muxer_thread_t::wait_for_stop() {
-  int rc = ten_thread_join(muxer_thread_, -1);
+  int rc = ten_thread_join(muxer_thread, -1);
   TEN_ASSERT(!rc, "Invalid argument.");
 
   TEN_LOGD("Muxer thread has been reclaimed.");
 }
 
-void muxer_thread_t::create_muxer_() {
-  muxer_ = new muxer_t();
-  TEN_ASSERT(muxer_, "Invalid argument.");
+void muxer_thread_t::create_muxer() {
+  muxer = new muxer_t();
+  TEN_ASSERT(muxer, "Invalid argument.");
 
-  muxer_->src_video_width_ = settings.src_video_width_;
-  muxer_->src_video_height_ = settings.src_video_height_;
-  muxer_->src_video_number_of_frames_ = settings.src_video_number_of_frames_;
-  muxer_->src_video_bit_rate_ = settings.src_video_bit_rate_;
-  muxer_->src_video_frame_rate_ = settings.src_video_frame_rate_;
-  muxer_->src_video_time_base_ = settings.src_video_time_base_;
+  muxer->src_video_width = settings.src_video_width;
+  muxer->src_video_height = settings.src_video_height;
+  muxer->src_video_number_of_frames = settings.src_video_number_of_frames;
+  muxer->src_video_bit_rate = settings.src_video_bit_rate;
+  muxer->src_video_frame_rate = settings.src_video_frame_rate;
+  muxer->src_video_time_base = settings.src_video_time_base;
 
-  muxer_->src_audio_sample_rate_ = settings.src_audio_sample_rate_;
-  muxer_->src_audio_time_base_ = settings.src_audio_time_base_;
-  muxer_->src_audio_channel_layout_ = settings.src_audio_channel_layout_;
+  muxer->src_audio_sample_rate = settings.src_audio_sample_rate;
+  muxer->src_audio_time_base = settings.src_audio_time_base;
+  muxer->src_audio_channel_layout = settings.src_audio_channel_layout;
 
-  muxer_->open(output_stream_, false);
+  muxer->open(output_stream, false);
 }
 
 // Put received audio frame from TEN world to FFmpeg.
 void muxer_thread_t::on_ten_audio_frame(
     std::unique_ptr<ten::audio_frame_t> frame) {
-  int rc = ten_mutex_lock(out_lock_);
+  int rc = ten_mutex_lock(out_lock);
   TEN_ASSERT(!rc, "Should not happen.");
 
-  // Discard one old video frame if the queue is full.
-  if (out_audios_.size() >= AUDIO_FRAME_FIFO_SIZE) {
-    out_audios_.pop_front();
-    TEN_LOGD("out_audios buffer overflow. One oldest audio frame is dropped");
-  }
+  out_audios.push_back(std::move(frame));
 
-  out_audios_.push_back(std::move(frame));
-
-  rc = ten_mutex_unlock(out_lock_);
+  rc = ten_mutex_unlock(out_lock);
   TEN_ASSERT(!rc, "Should not happen.");
 
-  ten_event_set(out_available_);
+  ten_event_set(out_available);
 }
 
 // Put received video frame from TEN world to FFmpeg.
 void muxer_thread_t::on_ten_video_frame(
     std::unique_ptr<ten::video_frame_t> frame) {
-  int rc = ten_mutex_lock(out_lock_);
+  int rc = ten_mutex_lock(out_lock);
   TEN_ASSERT(!rc, "Should not happen.");
 
-  // Discard one old video frame if the queue is full.
-  if (out_images_.size() >= VIDEO_FRAME_FIFO_SIZE) {
-    out_images_.pop_front();
-    TEN_LOGD("out_images buffer overflow. One oldest video frame is dropped.");
-  }
+  out_images.push_back(std::move(frame));
 
-  out_images_.push_back(std::move(frame));
-
-  rc = ten_mutex_unlock(out_lock_);
+  rc = ten_mutex_unlock(out_lock);
   TEN_ASSERT(!rc, "Should not happen.");
 
-  ten_event_set(out_available_);
+  ten_event_set(out_available);
 }
 
 }  // namespace ffmpeg_extension
