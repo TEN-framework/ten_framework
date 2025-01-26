d37d99ba3b6ec30a3c74bb2e7969b1115f09f24e
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-01-26 18:09:18 +0800

    fix: refine codes

--- tests/ten_runtime/integration/python/async_get_set_prop_2_python/async_get_set_prop_2_python_app/ten_packages/extension/default_async_extension_python/extension.py
+++ tests/ten_runtime/integration/python/async_get_set_prop_2_python/async_get_set_prop_2_python_app/ten_packages/extension/default_async_extension_python/extension.py
@@ -16,10 +16,16 @@ class DefaultAsyncExtension(AsyncExtension):
 
     async def on_init(self, ten_env: AsyncTenEnv) -> None:
         test_prop_json_str = await ten_env.get_property_to_json("test_prop")
-        print(test_prop_json_str)
         test_prop_json = json.loads(test_prop_json_str)
-        print(test_prop_json)
         assert test_prop_json == 32
+
+        test_prop_json_str = await ten_env.get_property_to_json()
+        test_prop_json = json.loads(test_prop_json_str)
+        assert test_prop_json["test_prop"] == 32
+
+        test_prop_json_str = await ten_env.get_property_to_json("")
+        test_prop_json = json.loads(test_prop_json_str)
+        assert test_prop_json["test_prop"] == 32
 
     async def on_cmd(self, ten_env: AsyncTenEnv, cmd: Cmd) -> None:
         # Send a new command to other extensions and wait for the result. The
