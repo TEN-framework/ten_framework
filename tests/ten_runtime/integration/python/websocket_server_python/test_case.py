bddf7460423595eb85611b45254436db8c96221d
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-01-27 14:03:42 +0800

    chore: refine integration test case python script (#624)

--- tests/ten_runtime/integration/python/websocket_server_python/test_case.py
+++ tests/ten_runtime/integration/python/websocket_server_python/test_case.py
@@ -164,8 +164,6 @@ def test_websocket_server_python():
         assert exit_code == 0
 
         if build_config_args.ten_enable_integration_tests_prebuilt is False:
-            source_root_path = os.path.join(base_path, app_dir_name)
-
             # Testing complete. If builds are only created during the testing
             # phase, we can clear the build results to save disk space.
             build_pkg.cleanup(app_root_path)
