01fb2275c7b478a3575d984b125c7acc2898fd3f
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-02-01 23:42:37 +0800

    feat: implement preferences dialog in tman designer

--- core/src/ten_manager/designer_frontend/src/constants/index.ts
+++ core/src/ten_manager/designer_frontend/src/constants/index.ts
@@ -6,3 +6,5 @@
 //
 export const TEN_FRAMEWORK_URL = "https://www.theten.ai/";
 export const TEN_FRAMEWORK_GITHUB_URL = "https://github.com/TEN-framework/";
+
+export const TEN_DEFAULT_APP_RUN_SCRIPT = "start";
