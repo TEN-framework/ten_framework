9c09b5160d3ead4f3179d6810f5b1ea45480c1d9
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-02-08 22:12:25 +0800

    fix: refine codes

--- tests/ten_runtime/integration/go/close_app_go/close_app_go_app/main_with_leak_check.go
+++ tests/ten_runtime/integration/go/close_app_go/close_app_go_app/main_with_leak_check.go
@@ -8,8 +8,13 @@
 
 package main
 
+// void __lsan_do_leak_check(void);
+import "C"
+
 import (
 	"fmt"
+	"runtime"
+	"runtime/debug"
 	"time"
 
 	"ten_framework/ten"
@@ -46,4 +51,20 @@ func main() {
 	go appRoutine(app, stopped)
 	go appTimeout(app, time.Duration(5*time.Second))
 	<-stopped
+
+	// A single GC is not enough; multiple rounds of GC are needed to clean up
+	// as thoroughly as possible.
+	for i := 0; i < 10; i++ {
+		// Explicitly trigger GC to increase the likelihood of finalizer
+		// execution.
+		debug.FreeOSMemory()
+		runtime.GC()
+
+		// Wait for a short period to give the GC time to run.
+		runtime.Gosched()
+		time.Sleep(1 * time.Second)
+	}
+
+	// To detect memory leaks with ASan, need to enable the following cgo code.
+	C.__lsan_do_leak_check()
 }
--- tests/ten_runtime/integration/go/start_app_sync_go/start_app_sync_go_app/main.go
+++ tests/ten_runtime/integration/go/start_app_sync_go/start_app_sync_go_app/main.go
@@ -1,18 +1,15 @@
+//
 // Copyright © 2025 Agora
 // This file is part of TEN Framework, an open source project.
 // Licensed under the Apache License, Version 2.0, with certain conditions.
 // Refer to https://github.com/TEN-framework/ten_framework/LICENSE for more
 // information.
-package main
+//
 
-// void __lsan_do_leak_check(void);
-import "C"
+package main
 
 import (
 	"fmt"
-	"runtime"
-	"runtime/debug"
-	"time"
 
 	"ten_framework/ten"
 )
@@ -47,20 +44,4 @@ func main() {
 	stopped := make(chan struct{}, 1)
 	go appRoutine(app, stopped)
 	<-stopped
-
-	// A single GC is not enough; multiple rounds of GC are needed to clean up
-	// as thoroughly as possible.
-	for i := 0; i < 10; i++ {
-		// Explicitly trigger GC to increase the likelihood of finalizer
-		// execution.
-		debug.FreeOSMemory()
-		runtime.GC()
-
-		// Wait for a short period to give the GC time to run.
-		runtime.Gosched()
-		time.Sleep(1 * time.Second)
-	}
-
-	// To detect memory leaks with ASan, need to enable the following cgo code.
-	C.__lsan_do_leak_check()
 }
