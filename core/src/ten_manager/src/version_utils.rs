3e133f2474cd400b9ba5a6583d1974b272587ea9
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-02-10 00:18:25 +0800

    chore: add more test cases for rust version compare

--- core/src/ten_manager/src/version_utils.rs
+++ core/src/ten_manager/src/version_utils.rs
@@ -18,3 +18,57 @@ pub fn parse_pkg_name_version_req(
         Ok((pkg_name_version.to_string(), VersionReq::STAR))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use semver::{Version, VersionReq};
+
+    #[test]
+    fn test_parse_version_rc() -> Result<()> {
+        let input = "aaa@3.0.0-rc1";
+        let (pkg, ver_req) = parse_pkg_name_version_req(input)?;
+        assert_eq!(pkg, "aaa");
+
+        let expected = VersionReq::parse("3.0.0-rc1")?;
+        assert_eq!(ver_req, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_pre_release_version_matches_1() {
+        let version_str = "0.8.0-rc.1";
+        let req_str = "^0.8.0-rc.1";
+
+        let req = VersionReq::parse(req_str)
+            .expect("Failed to parse version requirement");
+        let version =
+            Version::parse(version_str).expect("Failed to parse version");
+
+        assert!(
+            req.matches(&version),
+            "Version {:?} does not match requirement {}",
+            version,
+            req_str
+        );
+    }
+
+    #[test]
+    fn test_pre_release_version_matches_2() {
+        let version_str = "0.8.0-rc.1";
+        let req_str = "=0.8.0-rc.1";
+
+        let req = VersionReq::parse(req_str)
+            .expect("Failed to parse version requirement");
+        let version =
+            Version::parse(version_str).expect("Failed to parse version");
+
+        assert!(
+            req.matches(&version),
+            "Version {:?} does not match requirement {}",
+            version,
+            req_str
+        );
+    }
+}
