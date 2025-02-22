dfb6d7192777ff1d05a885013913fc1311d05adc
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-02-22 14:12:47 +0800

    fix: refine codes

--- build/ten_common/scripts/download.py
+++ build/ten_common/scripts/download.py
@@ -9,7 +9,7 @@ import requests
 
 
 def download_file(url, output_path):
-    response = requests.get(url, stream=True)
+    response = requests.get(url, stream=True, timeout=60)  # 60 seconds timeout.
     response.raise_for_status()
     with open(output_path, "wb") as file:
         for chunk in response.iter_content(chunk_size=8192):
