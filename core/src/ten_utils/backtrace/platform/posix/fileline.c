1b63296b166b70827dccd806f58eff2a768549d1
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-03-13 21:23:31 +0800

    fix: refine codes

--- core/src/ten_utils/backtrace/platform/posix/fileline.c
+++ core/src/ten_utils/backtrace/platform/posix/fileline.c
@@ -19,8 +19,8 @@
 #include <unistd.h>
 
 #include "include_internal/ten_utils/backtrace/backtrace.h"
-#include "include_internal/ten_utils/backtrace/file.h"
 #include "include_internal/ten_utils/backtrace/platform/posix/config.h" // IWYU pragma: keep
+#include "include_internal/ten_utils/backtrace/platform/posix/file.h"
 #include "include_internal/ten_utils/backtrace/platform/posix/internal.h"
 #include "ten_utils/lib/atomic_ptr.h"
 #include "ten_utils/macro/mark.h"
