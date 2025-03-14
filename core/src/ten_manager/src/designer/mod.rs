9e16d90f0e5a6d74af7c0688feaabe6e5e1841ae
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-03-14 14:17:13 +0800

    fix: refine codes

--- core/src/ten_manager/src/designer/mod.rs
+++ core/src/ten_manager/src/designer/mod.rs
@@ -9,6 +9,7 @@ mod app;
 pub mod builtin_function;
 mod common;
 mod dir_list;
+mod env;
 mod exec;
 mod file_content;
 pub mod frontend;
@@ -52,6 +53,7 @@ pub fn configure_routes(
                 "/check-update",
                 web::get().to(version::check_update_endpoint),
             )
+            .route("/env", web::get().to(env::get_env))
             .route("/addons", web::post().to(addons::get_addons))
             .route(
                 "/packages/reload",
