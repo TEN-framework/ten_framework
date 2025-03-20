92f12e5e245a319bf27f10141e7d3b30ad26df65
czhen <56986964+shczhen@users.noreply.github.com>
2025-03-20 18:02:59 +0800

    fix: apply addon API

--- core/src/ten_manager/designer_frontend/src/api/services/addons.ts
+++ core/src/ten_manager/designer_frontend/src/api/services/addons.ts
@@ -8,17 +8,6 @@ import { makeAPIRequest } from "@/api/services/utils";
 import { ENDPOINT_ADDONS } from "@/api/endpoints";
 import { ENDPOINT_METHOD } from "@/api/endpoints/constant";
 
-export const getExtensionAddonByName = async (name: string) => {
-  const template = ENDPOINT_ADDONS.extensionByName[ENDPOINT_METHOD.GET];
-  const req = makeAPIRequest(template, {
-    pathParams: {
-      name,
-    },
-  });
-  const res = await req;
-  return template.responseSchema.parse(res).data;
-};
-
 export const retrieveAddons = async (payload: {
   base_dir?: string;
   addon_name?: string;
