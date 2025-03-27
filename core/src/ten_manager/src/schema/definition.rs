b21a4b7b41fa6b3a4d0491dd84d0cef2db139221
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-03-27 14:47:13 +0800

    fix: refine codes

--- core/src/ten_manager/src/schema/definition.rs
+++ core/src/ten_manager/src/schema/definition.rs
@@ -5,5 +5,7 @@
 // Refer to the "LICENSE" file in the root directory for more information.
 //
 
+pub const DESIGNER_SCHEMA: &str = include_str!("./data/designer.schema.json");
+
 pub const TMAN_CONFIG_SCHEMA: &str =
     include_str!("./data/config_file.schema.json");
