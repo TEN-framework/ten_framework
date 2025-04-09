88b0e6af30a4876a7f73950cb1fe20719edbfb84
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-04-09 16:47:31 +0800

    refactor: clean up get_id method in GraphInfo struct and improve comments in extension.rs (#955)

--- core/src/ten_rust/src/graph/graph_info.rs
+++ core/src/ten_rust/src/graph/graph_info.rs
@@ -30,30 +30,6 @@ pub struct GraphInfo {
 }
 
 impl GraphInfo {
-    pub fn get_id(&self) -> String {
-        let mut id = String::new();
-
-        // Add app_base_dir prefix if it exists.
-        if let Some(base_dir) = &self.app_base_dir {
-            id.push_str(base_dir);
-            id.push('-');
-        }
-
-        // Add belonging_pkg_type.
-        id.push_str(&format!("{}-", self.belonging_pkg_type));
-
-        // Add belonging_pkg_name if it exists.
-        if let Some(pkg_name) = &self.belonging_pkg_name {
-            id.push_str(pkg_name);
-            id.push('-');
-        }
-
-        // Add graph_name.
-        id.push_str(&self.name);
-
-        id
-    }
-
     pub fn validate_and_complete(&mut self) -> Result<()> {
         self.graph.validate_and_complete()
     }
