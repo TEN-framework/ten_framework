ccadec5a12e1e9360b34e066651f6c476b044dd5
czhen <56986964+shczhen@users.noreply.github.com>
2025-04-10 17:38:18 +0800

    fix: rm legacy code Graph

--- core/src/ten_manager/designer_frontend/src/components/Popup/Graph.tsx
+++ core/src/ten_manager/designer_frontend/src/components/Popup/Graph.tsx
@@ -7,7 +7,6 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
 
-import { PopupBase } from "@/components/Popup/Base";
 import { useWidgetStore } from "@/store/widget";
 import {
   GraphAddConnectionWidget,
@@ -18,9 +17,6 @@ import { EGraphActions } from "@/types/graphs";
 
 import type { IGraphWidget } from "@/types/widgets";
 import { TCustomNode } from "@/types/flow";
-
-const DEFAULT_WIDTH = 400;
-const DEFAULT_HEIGHT = 300;
 
 export const GraphPopupTitle = (props: {
   type: EGraphActions;
@@ -83,75 +79,5 @@ export const GraphPopupContent = (props: { widget: IGraphWidget }) => {
         />
       )}
     </>
-  );
-};
-
-/** @deprecated */
-export const GraphPopup = (props: {
-  id: string;
-  metadata: IGraphWidget["metadata"];
-}) => {
-  const { id, metadata } = props;
-
-  const { t } = useTranslation();
-  const { removeWidget } = useWidgetStore();
-
-  const handleClose = () => {
-    removeWidget(id);
-  };
-
-  const titleMemo = React.useMemo(() => {
-    switch (metadata.type) {
-      case EGraphActions.ADD_NODE:
-        return t("popup.graph.titleAddNode");
-      case EGraphActions.ADD_CONNECTION:
-        return t("popup.graph.titleAddConnection");
-      case EGraphActions.UPDATE_NODE_PROPERTY:
-        return t("popup.graph.titleUpdateNodePropertyByName", {
-          name: metadata.node?.data.name,
-        });
-      default:
-        return t("popup.graph.title");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [metadata.type, metadata.node]);
-
-  return (
-    <PopupBase
-      id={id}
-      title={titleMemo}
-      onClose={handleClose}
-      resizable={true}
-      width={DEFAULT_WIDTH}
-      height={DEFAULT_HEIGHT}
-      contentClassName="px-0"
-    >
-      {metadata.type === EGraphActions.ADD_NODE && (
-        <GraphAddNodeWidget
-          {...metadata}
-          postAddNodeActions={() => {
-            removeWidget(id);
-          }}
-        />
-      )}
-      {metadata.type === EGraphActions.ADD_CONNECTION && (
-        <GraphAddConnectionWidget
-          {...metadata}
-          postAddConnectionActions={() => {
-            removeWidget(id);
-          }}
-        />
-      )}
-      {metadata.type === EGraphActions.UPDATE_NODE_PROPERTY &&
-        metadata.node && (
-          <GraphUpdateNodePropertyWidget
-            {...metadata}
-            node={metadata.node}
-            postUpdateNodePropertyActions={() => {
-              removeWidget(id);
-            }}
-          />
-        )}
-    </PopupBase>
   );
 };
