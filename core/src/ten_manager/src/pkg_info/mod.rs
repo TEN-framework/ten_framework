fd8e85efe1509b16e591bc5ba0a1f9912e217fb0
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-04-11 23:26:04 +0800

    fix: refine codes

--- core/src/ten_manager/src/pkg_info/mod.rs
+++ core/src/ten_manager/src/pkg_info/mod.rs
@@ -12,7 +12,10 @@ use std::{path::Path, sync::Arc};
 use anyhow::Result;
 
 use crate::{config::TmanConfig, output::TmanOutput};
-use ten_rust::pkg_info::{get_app_installed_pkgs, PkgInfo};
+use ten_rust::{
+    base_dir_pkg_info::PkgsInfoInApp,
+    pkg_info::{get_app_installed_pkgs, pkg_type::PkgType, PkgInfo},
+};
 
 /// Retrieves information about all installed packages for the specified
 /// application.
@@ -30,4 +33,27 @@ pub fn tman_get_all_installed_pkgs_info_of_app(
         out.normal_line(&format!("{:?}", all_pkgs));
     }
     Ok(all_pkgs)
+}
+
+pub fn get_pkg_in_app<'a>(
+    pkgs_info_in_app: &'a PkgsInfoInApp,
+    pkg_type: &PkgType,
+    pkg_name: &Option<&String>,
+) -> Result<Option<&'a PkgInfo>> {
+    match pkg_type {
+        PkgType::App => Ok(pkgs_info_in_app.app_pkg_info.as_ref()),
+        PkgType::Extension => {
+            assert!(pkg_name.is_some());
+
+            match &pkgs_info_in_app.extension_pkg_info {
+                Some(pkgs) => Ok(pkgs.iter().find(|pkg| {
+                    pkg.manifest.as_ref().is_some_and(|m| {
+                        m.type_and_name.name == *pkg_name.unwrap()
+                    })
+                })),
+                None => Ok(None),
+            }
+        }
+        _ => Err(anyhow::anyhow!("Unsupported package type: {:?}", pkg_type)),
+    }
 }
