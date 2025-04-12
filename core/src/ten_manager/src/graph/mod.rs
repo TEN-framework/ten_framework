2bc6b3b2e8f109f8d52843fb3ab71cefaaae696f
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-04-12 11:18:42 +0800

    feat: add functions to find graphs by name and ID in cache

--- core/src/ten_manager/src/graph/mod.rs
+++ core/src/ten_manager/src/graph/mod.rs
@@ -7,5 +7,35 @@
 pub mod connections;
 pub mod nodes;
 
+use std::collections::HashMap;
+use uuid::Uuid;
+
 pub use connections::update_graph_connections_all_fields;
 pub use nodes::update_graph_node_all_fields;
+
+use ten_rust::graph::graph_info::GraphInfo;
+
+pub fn graphs_cache_find_by_name<'a>(
+    graphs_cache: &'a HashMap<Uuid, GraphInfo>,
+    graph_name: &str,
+) -> Option<&'a GraphInfo> {
+    graphs_cache.iter().find_map(|(_, graph_info)| {
+        if graph_info
+            .name
+            .as_ref()
+            .map(|name| name == graph_name)
+            .unwrap_or(false)
+        {
+            Some(graph_info)
+        } else {
+            None
+        }
+    })
+}
+
+pub fn graphs_cache_find_by_id(
+    graphs_cache: &HashMap<Uuid, GraphInfo>,
+    graph_id: Uuid,
+) -> Option<&GraphInfo> {
+    graphs_cache.get(&graph_id)
+}
