35372220f2310bbbba3c796cbd4ca7d318f09408
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-04-16 22:34:17 +0800

    refactor: improve variable naming for clarity in addon management (#1002)

--- core/include_internal/ten_runtime/addon_loader/addon_loader.h
+++ core/include_internal/ten_runtime/addon_loader/addon_loader.h
@@ -61,8 +61,8 @@ typedef struct ten_addon_loader_t {
   ten_env_t *ten_env;
 } ten_addon_loader_t;
 
-TEN_RUNTIME_API bool ten_addon_loader_check_integrity(
-    ten_addon_loader_t *self, bool check_thread);
+TEN_RUNTIME_API bool ten_addon_loader_check_integrity(ten_addon_loader_t *self,
+                                                      bool check_thread);
 
 TEN_RUNTIME_API ten_env_t *ten_addon_loader_get_ten_env(
     ten_addon_loader_t *self);
