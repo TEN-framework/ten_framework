be7320ac9249aba2a1e67ff0fae7d5a32df5e8e7
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-04-17 10:07:57 +0800

    refactor: simplify file writing logic by using BufWriter for improved performance in unpackage.rs

--- core/src/ten_manager/src/package_file/unpackage.rs
+++ core/src/ten_manager/src/package_file/unpackage.rs
@@ -17,6 +17,7 @@ use flate2::read::GzDecoder;
 use tar::Archive as TarArchive;
 use zip::ZipArchive;
 
+use crate::constants::BUF_WRITER_BUF_SIZE;
 use crate::file_type::detect_file_type;
 use crate::install::installed_paths::{sort_installed_paths, InstalledPaths};
 use crate::install::template::{
@@ -96,7 +97,8 @@ fn extract_and_process_zip_normal_part(
             }
 
             let out_file = File::create(&out_path)?;
-            let mut buffered_writer = BufWriter::new(out_file);
+            let mut buffered_writer =
+                BufWriter::with_capacity(BUF_WRITER_BUF_SIZE, out_file);
             io::copy(&mut file, &mut buffered_writer)?;
 
             #[cfg(unix)]
@@ -243,7 +245,8 @@ fn extract_and_process_tar_gz_normal_part(
             }
 
             let out_file = File::create(&out_path)?;
-            let mut buffered_writer = BufWriter::new(out_file);
+            let mut buffered_writer =
+                BufWriter::with_capacity(BUF_WRITER_BUF_SIZE, out_file);
             io::copy(&mut entry, &mut buffered_writer)?;
 
             #[cfg(unix)]
