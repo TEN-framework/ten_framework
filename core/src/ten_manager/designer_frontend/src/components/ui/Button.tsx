c5cacc7e8080009386cec0379fdf7f4240ac6a46
czhen <56986964+shczhen@users.noreply.github.com>
2025-04-17 14:57:24 +0800

    feat: init design

--- core/src/ten_manager/designer_frontend/src/components/ui/Button.tsx
+++ core/src/ten_manager/designer_frontend/src/components/ui/Button.tsx
@@ -29,9 +29,11 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        default: "h-base px-4 py-2",
+        xs: "h-xs rounded-md px-2 text-xs [_svg]:size-3",
+        sm: "h-sm rounded-md px-3 text-xs",
+        lg: "h-lg rounded-md px-8",
+        xl: "h-xl rounded-md px-8",
         icon: "h-8 w-8",
         status:
           "h-5 w-fit px-2 [&_svg]:size-3 cursor-pointer rounded-none text-xs gap-1",
